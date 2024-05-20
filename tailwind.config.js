2188b6296bb5b08daadafba83f82dacfbfdba2b0
uyamini <uyamini1996@gmail.com>
2024-05-20 05:38:01 -0500

    add some tailwind css (not working)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,26 @@ module.exports = {
   purge: [
     './templates/**/*.html',
     './main_app/templates/**/*.html',
+    './theme/static/src/**/*.css',
   ],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        indigo: {
+           50: '#edf2ff',
+           100: '#dbe4ff',
+           200: '#bac8ff',
+           300: '#91a7ff',
+           400: '#748ffc',
+           500: '#5c7cfa',
+           600: '#4c6ef5',
+           700: '#4263eb',
+           800: '#3b5bdb',
+           900: '#364fc7',
+         },
+      },
+    },
   },
   variants: {
     extend: {},
