4d2af3b5cba1994b3904c121b2558f9a39592837
uyamini <uyamini1996@gmail.com>
2024-05-29 00:36:13 -0500

    Config deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn purpose.wsgi
--- purpose/settings.py
+++ purpose/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import environ
 env = environ.Env()
 env.read_env()
+import django_on_heroku
+django_on_heroku.settings(locals(), databases=False)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
